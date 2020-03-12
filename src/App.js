77b3178d7c274bab6f0134ed75437e7ab3ba6ded
Spicey Brisket <spiceybrisket@pop-os.localdomain>
2020-03-13 08:45:35 +1300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import "./assets/main.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex h-screen justify-center overflow-hidden">
+      <div className="hidden md:block">
+        <img className="h-screen" src="images/fox.png" alt="" />
+      </div>
+      <div className="w-full flex flex-col justify-start ">
+        <div className="mt-4 mx-8 md:mx-0 md:w-full flex justify-center items-center">
+          <img
+            className="h-32 w-auto md:hidden object-cover "
+            src="images/fox-r.png"
+            alt=""
+          />
+          <img
+            className="h-32 w-auto md:w-auto md:h-auto md:mt-8 object-cover"
+            src="/images/logo.png"
+            alt=""
+          />
+        </div>
+        <div className="mt-8 mx-8 text-left "></div>
+      </div>
     </div>
   );
 }
